08add07377bf9d961d72b1a46f03a599b70e4d04
labrac <raquelortegalabrac@hotmail.com>
2023-05-24 22:07:05 +0200

    Add character.ts

---
+++ src/models/character.ts
@@ -0,0 +1,15 @@
+export class Character {
+  static series = "Game of Thrones";
+
+  name: string;
+  house: string;
+  age: number;
+  alive: boolean;
+  sentence: string;
+
+  constructor(name: string, family: string, age: number) {
+    this.name = name;
+    this.house = family;
+    this;
+  }
+}
